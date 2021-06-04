149e9d46117694abc4a99cc006d9176d78fcf9e3
Mike <k2punk@gmail.com>
2021-06-04 16:40:08 +0300

    rewrite third step with high ordered functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/mkolotovich/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/mkolotovich/frontend-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/f7687a3f327fe7d6db80/maintainability)](https://codeclimate.com/github/mkolotovich/frontend-project-lvl2/maintainability)
+[![Actions Status](https://github.com/mkolotovich/frontend-project-lvl2/workflows/ESLint-check/badge.svg)](https://github.com/mkolotovich/frontend-project-lvl2/actions)
 [![asciicast](https://asciinema.org/a/l0l2DsCLbGOMTNgIku6pMG05D.svg)](https://asciinema.org/a/l0l2DsCLbGOMTNgIku6pMG05D)
--- compareFiles.js
+++ compareFiles.js
@@ -1,44 +1,42 @@
 import * as fs from 'fs';
-// import _ from 'lodash';
-
-const compareFiles = (file1, file2) => {
-  const firstFile = JSON.parse(fs.readFileSync(file1, 'utf8'));
-  const secondFile = JSON.parse(fs.readFileSync(file2, 'utf8'));
-  const result = {};
-  const entries = Object.entries(firstFile).sort();
-  const entries1 = Object.entries(secondFile).sort();
-  for (const [prop, ] of entries) {
-    for (const [prop1, value1] of entries1) {
-      if (firstFile.hasOwnProperty(prop1) && firstFile[prop1] === value1) {
-        result[`  ${prop1}`] = value1;
-      } else {
-        if (firstFile.hasOwnProperty(prop1) && firstFile[prop1] !== value1) {
-          result[`- ${prop1}`] = firstFile[prop1];
-          result[`+ ${prop1}`] = value1;
-        } 
-        else if (!secondFile.hasOwnProperty(prop)) {
-          result[`- ${prop}`] = firstFile[prop];
-        }
-        else {
-          result[`+ ${prop1}`] = value1;
-        }
-      } 
-    }
-  }
-  const resultArray = Object.entries(result).sort(compare);
-  const sortedObject = Object.fromEntries(resultArray);
-  const resultString = JSON.stringify(sortedObject, null, '  ');
-  const resultValue = resultString.replace(/"/g,'').replace(/,/g,'');
-  return resultValue;
-};
-
-export default compareFiles;
+import _ from 'lodash';
 
 const compare = (a, b) => {
-  const firstWord = [a[0].slice(0, 2),a[0].slice(2)];
-  const secondWord = [b[0].slice(0, 2),b[0].slice(2)];
+  const firstWord = [a[0].slice(0, 2), a[0].slice(2)];
+  const secondWord = [b[0].slice(0, 2), b[0].slice(2)];
   if (firstWord[1] === secondWord[1]) {
     return 0;
   }
   return firstWord[1] > secondWord[1] ? 1 : -1;
 };
+
+const compareFiles = (file1, file2) => {
+  const firstFile = JSON.parse(fs.readFileSync(file1, 'utf8'));
+  const secondFile = JSON.parse(fs.readFileSync(file2, 'utf8'));
+  const entries = Object.entries(firstFile).sort();
+  const entries1 = Object.entries(secondFile).sort();
+  const cb = (acc) => {
+    entries.map(([prop]) => entries1.map(([prop1, value1]) => {
+      if (_.has(firstFile, prop1) && firstFile[prop1] === value1) {
+        acc[`  ${prop1}`] = value1;
+      } else if (_.has(firstFile, prop1) && firstFile[prop1] !== value1) {
+        acc[`- ${prop1}`] = firstFile[prop1];
+        acc[`+ ${prop1}`] = value1;
+      } else if (!_.has(secondFile, prop)) {
+        acc[`- ${prop}`] = firstFile[prop];
+      } else {
+        acc[`+ ${prop1}`] = value1;
+      }
+      return acc;
+    }));
+    return acc;
+  };
+  const res = entries.reduce(cb, {});
+  const resultArray = Object.entries(res).sort(compare);
+  const sortedObject = Object.fromEntries(resultArray);
+  const resultString = JSON.stringify(sortedObject, null, '  ');
+  const resultValue = resultString.replace(/"/g, '').replace(/,/g, '');
+  return resultValue;
+};
+
+export default compareFiles;
