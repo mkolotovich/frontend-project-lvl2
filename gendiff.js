3434447f554cb7e243159b29bc0a093f1f230e6b
Mike <k2punk@gmail.com>
2021-10-28 15:45:24 +0300

    fix gendiff file

--- gendiff.js
+++ gendiff.js
@@ -12,5 +12,6 @@ program
   .arguments('<filepath2>')
   .action((filepath1, filepath2) => {
     console.log(parseFiles(filepath1, filepath2, program.opts().format));
-  })
-  .parse();
+  });
+
+program.parse();
