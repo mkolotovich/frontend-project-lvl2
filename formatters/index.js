d2732c21a2d441c30b9b2785ac35468f242197a3
Mike <k2punk@gmail.com>
2021-10-28 14:49:26 +0300

    write json formatter

--- formatters/index.js
+++ formatters/index.js
@@ -1,9 +1,12 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const chooseFormatter = (formatName, structre) => {
   if (formatName === 'plain') {
     return plain(structre, '');
+  } if (formatName === 'json') {
+    return json(structre);
   }
   return stylish(structre);
 };
---
+++ formatters/json.js
@@ -0,0 +1,162 @@
+import _ from 'lodash';
+
+let string = '{\n';
+let spaceSize = 2;
+
+const makeSpace = (size, space) => {
+  let spaceType = space;
+  if (size > 0) {
+    spaceType += ' ';
+    return makeSpace(size - 1, spaceType);
+  }
+  return space;
+};
+
+const makeLine = (name) => {
+  string += `${makeSpace(spaceSize, '')}"${name}": `;
+  string += '{\n';
+  spaceSize += 2;
+};
+
+const res = (json, children, tail) => json(children) + json(tail);
+
+const json = (tree) => {
+  const cb = () => {
+    if (!Array.isArray(tree)) {
+      if (Object.entries(tree).length === 1) {
+        const [name, value] = Object.entries(tree)[0];
+        string += `${makeSpace(spaceSize, '')}"${name}": "${value}"\n`;
+        spaceSize -= 2;
+        string += `${makeSpace(spaceSize, '')}},\n`;
+      } else {
+        const [name, value] = Object.entries(tree)[0];
+        if (!_.isObject(value)) {
+          string += `${makeSpace(spaceSize, '')}"${name}": ${value},\n`;
+          const [name1, value1] = Object.entries(tree)[1];
+          string += `${makeSpace(spaceSize, '')}"${name1}": {\n`;
+          spaceSize += 2;
+          const [subName, subValue] = Object.entries(value1)[0];
+          string += `${makeSpace(spaceSize, '')}"${subName}": ${subValue}\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}}\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}},\n`;
+        } else {
+          string += `${makeSpace(spaceSize, '')}"${name}": {\n`;
+          spaceSize += 2;
+          const subName = Object.keys(value);
+          string += `${makeSpace(spaceSize, '')}"${subName}": {\n`;
+          const [subSubName, subValue] = Object.entries(value[subName])[0];
+          spaceSize += 2;
+          string += `${makeSpace(spaceSize, '')}"${subSubName}": ${subValue}\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}}\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}},\n`;
+          const [name1, value1] = Object.entries(tree)[1];
+          string += `${makeSpace(spaceSize, '')}"${name1}": ${value1}\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}}\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}}`;
+        }
+      }
+      return string;
+    }
+    if (tree.length === 0) {
+      return string;
+    }
+    const [head, ...tail] = tree;
+    const [name, type] = head;
+    const children = head[2];
+    if (type === 'unchanged' && head[2] instanceof Array) {
+      makeLine(name);
+      return json(children) + json(tail);
+    } if (type === 'added' && head[2] instanceof Object) {
+      makeLine(name);
+      res(json, children, tail);
+    } else if (type === 'deleted' && head[2] instanceof Object) {
+      makeLine(name);
+      res(json, children, tail);
+    } else if (type === 'added' && head[2] instanceof Array) {
+      spaceSize += 2;
+      makeLine(name, '+');
+      res(json, children, tail);
+    } else if (type === 'deleted' && head[2] instanceof Array) {
+      makeLine(name, '-');
+      res(json, children, tail);
+    } else if (type === 'added' && typeof head[2] === 'string') {
+      if (tail.length === 0) {
+        string += `${makeSpace(spaceSize, '')}"${name}": "${head[2]}"\n`;
+        spaceSize -= 2;
+        string += `${makeSpace(spaceSize, '')}}\n`;
+        spaceSize -= 2;
+        string += `${makeSpace(spaceSize, '')}},\n`;
+      } else {
+        string += `${makeSpace(spaceSize, '')}"${name}": "${head[2]}",\n`;
+      }
+      json(tail);
+    } else if (type === 'added') {
+      if (tail.length !== 0) {
+        string += `${makeSpace(spaceSize, '')}"${name}": ${head[2]},\n`;
+      } else {
+        string += `${makeSpace(spaceSize, '')}+ ${name}: ${head[2]}\n`;
+        spaceSize -= 2;
+        string += `${makeSpace(spaceSize, '')}}\n`;
+        spaceSize -= 4;
+        string += `${makeSpace(spaceSize, '')}}\n`;
+        spaceSize -= 2;
+      }
+      json(tail);
+    } else if (type === 'unchanged') {
+      string += `${makeSpace(spaceSize, '')}"${name}": "${head[2]}",\n`;
+      json(tail);
+    } else if (type === 'deleted') {
+      string += `${makeSpace(spaceSize, '')}"${name}": ${head[2]},\n`;
+      json(tail);
+    } else if (type === 'changed') {
+      if (tail.length !== 0) {
+        string += `${makeSpace(spaceSize, '')}"${name}": [\n`;
+        spaceSize += 2;
+        if (typeof head[2] === 'string') {
+          string += `${makeSpace(spaceSize, '')}"${head[2]}",\n`;
+          string += `${makeSpace(spaceSize, '')}"${head[3]}"\n`;
+        } else {
+          string += `${makeSpace(spaceSize, '')}${head[2]},\n`;
+          string += `${makeSpace(spaceSize, '')}${head[3]}\n`;
+        }
+        spaceSize -= 2;
+        string += `${makeSpace(spaceSize, '')}],\n`;
+      } else {
+        if (head[2] instanceof Array) {
+          string += `${makeSpace(spaceSize, '')}"${name}": [\n`;
+          spaceSize += 2;
+          string += `${makeSpace(spaceSize, '')}{\n`;
+          spaceSize += 2;
+          const [subName, , value] = head[2];
+          string += `${makeSpace(spaceSize, '')}"${subName}": "${value}"\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}},\n`;
+          string += `${makeSpace(spaceSize, '')}"${head[3]}"\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}]\n`;
+        } else {
+          string += `${makeSpace(spaceSize, '')}"${name}": [\n`;
+          spaceSize += 2;
+          string += `${makeSpace(spaceSize, '')}"${head[2]}",\n`;
+          string += `${makeSpace(spaceSize, '')}"${head[3]}"\n`;
+          spaceSize -= 2;
+          string += `${makeSpace(spaceSize, '')}]\n`;
+        }
+        spaceSize -= 2;
+        string += `${makeSpace(spaceSize, '')}},\n`;
+      }
+      json(tail);
+    }
+    return string;
+  };
+  cb();
+  return string;
+};
+
+export default json;
