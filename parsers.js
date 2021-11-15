2e46c701c87bbee32b061152b4c02cdcb7007a58
Mike <k2punk@gmail.com>
2021-11-15 17:17:00 +0300

    remove commented code in parseFiles function

--- parsers.js
+++ parsers.js
@@ -9,15 +9,9 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.resolve(__dirname, '__fixtures__', filename);
 
 const parseFiles = (file1, file2, formatName) => {
-  // let parse;
   const fileExt1 = path.extname(file1);
   const fileExt2 = path.extname(file2);
   const parse = fileExt1 === '.json' && fileExt2 === '.json' ? JSON.parse : yaml.load;
-  // if (fileExt1 === '.json' && fileExt2 === '.json') {
-  //   parse = JSON.parse;
-  // } if ((fileExt1 === '.yml' || fileExt1 === '.yaml') && (fileExt2 === '.yml' || fileExt2 === '.yaml')) {
-  //   parse = yaml.load;
-  // }
   return compareFiles(parse(fs.readFileSync(getFixturePath(file1), 'utf8')), parse(fs.readFileSync(getFixturePath(file2), 'utf8')), formatName);
 };
 
