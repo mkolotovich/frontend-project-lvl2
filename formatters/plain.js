3a20dbb7cc075325984e235b208ae6b72e4de7b4
Mike <k2punk@gmail.com>
2021-11-19 12:36:49 +0300

    add isString function

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,6 +1,19 @@
 import _ from 'lodash';
 import { isLeaf } from './stylish.js';
 
+const isString = (value, newValue) => {
+  if (typeof value === 'string' && typeof newValue === 'string') {
+    return [`'${value}'`, `'${newValue}'`];
+  }
+  if (typeof value === 'string') {
+    return [`'${value}'`, newValue];
+  }
+  if (typeof newValue === 'string') {
+    return [value, `'${newValue}'`];
+  }
+  return [value, newValue];
+};
+
 const plain = (tree, result, path = '') => {
   const {
     name, value, status, newValue, children,
@@ -13,12 +26,10 @@ const plain = (tree, result, path = '') => {
       return `${result}Property '${nodeName}' was added with value: ${value}\n`;
     } if (status === 'removed') return `${result}Property '${nodeName}' was removed\n`;
     if (status === 'updated') {
-      if (typeof value === 'string' && typeof newValue === 'string') return `${result}Property '${nodeName}' was updated. From '${value}' to '${newValue}'\n`;
-      if (typeof value === 'string') return `${result}Property '${nodeName}' was updated. From '${value}' to ${newValue}\n`;
+      const [printValue, printNewValue] = isString(value, newValue);
       if (_.isObject(value)) return `${result}Property '${nodeName}' was updated. From [complex value] to '${newValue}'\n`;
-      if (typeof newValue === 'string') return `${result}Property '${nodeName}' was updated. From ${value} to '${newValue}'\n`;
       if (_.isObject(newValue)) return `${result}Property '${nodeName}' was updated. From ${value} to [complex value]\n`;
-      return `${result}Property '${nodeName}' was updated. From ${value} to ${newValue}\n`;
+      return `${result}Property '${nodeName}' was updated. From ${printValue} to ${printNewValue}\n`;
     }
     return '';
   }
