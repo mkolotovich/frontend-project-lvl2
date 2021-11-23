6f5d8e8a5e66190745ca5243702e79a0cdb6383e
Mike <k2punk@gmail.com>
2021-11-23 15:19:54 +0300

    remove extra condition in stylish function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -17,15 +17,15 @@ const replacer = (obj, size, acc = '') => {
   const [key, value] = head;
   if (_.isObject(value)) {
     if (tail) {
-      return replacer(value, size + 4, `${acc}${makeSpace(size + 4, '')}  ${key}: {\n`) + replacer(Object.fromEntries([tail]), size, `${makeSpace(size + 10, '')}}\n${makeSpace(size + 6, '')}}\n`);
+      return replacer(value, size + depthSpaceSize, `${acc}${makeSpace(size + depthSpaceSize, '')}${key}: {\n`) + replacer(Object.fromEntries([tail]), size, `${makeSpace(size + 8, '')}}\n${makeSpace(size + depthSpaceSize, '')}}\n`);
     }
-    return replacer(value, size + 4, `${acc}${makeSpace(size + 4, '')}  ${key}: {\n`);
+    return replacer(value, size + depthSpaceSize, `${acc}${makeSpace(size + depthSpaceSize, '')}${key}: {\n`);
   }
   if (tail !== undefined) {
-    const result = `${acc}${makeSpace(size + 4, '')}  ${key}: ${value}\n`;
-    return result + replacer(Object.fromEntries([tail]), size, acc);
+    const result = `${acc}${makeSpace(size + depthSpaceSize, '')}  ${key}: ${value}\n`;
+    return result + replacer(Object.fromEntries([tail]), size + spaceSize, acc);
   }
-  return `${acc}${makeSpace(size + 4, '')}  ${key}: ${value}\n`;
+  return `${acc}${makeSpace(size + depthSpaceSize, '')}${key}: ${value}\n`;
 };
 
 const makeLine = (item, depth) => {
@@ -37,10 +37,10 @@ const makeLine = (item, depth) => {
 
 const printComplexValues = (value, newValue, depth) => {
   if (_.isObject(value)) {
-    return [`{\n${replacer(value, spaceSize * depth + spaceSize)}${makeSpace(spaceSize * depth + 4, '')}}`, newValue];
+    return [`{\n${replacer(value, depthSpaceSize * depth)}${makeSpace(depthSpaceSize * depth, '')}}`, newValue];
   }
   if (_.isObject(newValue)) {
-    return [value, `{\n${replacer(newValue, spaceSize * depth + spaceSize)}${makeSpace(spaceSize * depth + 4, '')}}`];
+    return [value, `{\n${replacer(newValue, depthSpaceSize * depth)}${makeSpace(depthSpaceSize * depth, '')}}`];
   }
   return [value, newValue];
 };
@@ -50,17 +50,16 @@ export const stylish = (data, result, depth = 0) => {
     name, value, status, newValue, children,
   } = data;
   if (isLeaf(data)) {
+    const [printValue, printNewValue] = printComplexValues(value, newValue, depth);
     if (status === 'added') {
-      if (_.isObject(value)) return `${result}${makeSpace(depthSpaceSize * (depth - 1) + spaceSize, '')}+ ${name}: {\n${replacer(value, depthSpaceSize * (depth - 1) + spaceSize)}${makeSpace(depthSpaceSize * depth, '')}}\n`;
-      return `${result}${makeSpace(spaceSize ** depth + 2, '')}+ ${name}: ${value}\n`;
+      return `${result}${makeSpace(depthSpaceSize * (depth - 1) + spaceSize, '')}+ ${name}: ${printValue}\n`;
     } if (status === 'removed') {
       if (_.isObject(value)) return `${result}${makeSpace(spaceSize, '')}- ${name}: {\n${replacer(value, spaceSize * depth)}${makeSpace(spaceSize * depth + 6, '')}}\n${makeSpace(spaceSize * depth + 2, '')}}\n`;
       return `${result}${makeSpace(depthSpaceSize * (depth - 1) + spaceSize, '')}- ${name}: ${value}\n`;
     } if (status === 'updated') {
-      const [printValue, printNewValue] = printComplexValues(value, newValue, depth);
       return `${result}${makeSpace(depthSpaceSize * (depth - 1) + spaceSize, '')}- ${name}: ${printValue}\n${result}${makeSpace(depthSpaceSize * (depth - 1) + spaceSize, '')}+ ${name}: ${printNewValue}\n`;
     }
-    return `${result}${makeSpace(spaceSize ** depth + 2, '')}  ${name}: ${value}\n`;
+    return `${result}${makeSpace(spaceSize ** depth + spaceSize, '')}  ${name}: ${value}\n`;
   }
   const line = children.map((item) => stylish(item, makeLine(item, depth + 1), depth + 1));
   if (name === '') return `{\n${result}${line.join('')}${makeSpace(spaceSize * depth * spaceSize, '')}}`;
