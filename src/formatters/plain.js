758bcfeeda14dc005ca5b9d69e4547282a96d31d
Mike <k2punk@gmail.com>
2021-12-21 13:51:54 +0300

    refactor plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,7 +1,36 @@
 import _ from 'lodash';
 import { isLeaf } from './stylish.js';
 
-const printSimpleValues = (value, newValue) => {
+// const printSimpleValues = (value, newValue) => {
+//   if (typeof value === 'string' && typeof newValue === 'string') {
+//     return [`'${value}'`, `'${newValue}'`];
+//   }
+//   if (typeof value === 'string') {
+//     return [`'${value}'`, newValue];
+//   }
+//   if (typeof newValue === 'string') {
+//     return [value, `'${newValue}'`];
+//   }
+//   return [value, newValue];
+// };
+
+// const printComplexValues = (value, newValue) => {
+//   if (_.isObject(value)) {
+//     return ['[complex value]', `'${newValue}'`];
+//   }
+//   if (_.isObject(newValue)) {
+//     return [value, '[complex value]'];
+//   }
+//   return [value, newValue];
+// };
+
+const printValues = (value, newValue) => {
+  if (_.isObject(value)) {
+    return ['[complex value]', `'${newValue}'`];
+  }
+  if (_.isObject(newValue)) {
+    return [value, '[complex value]'];
+  }
   if (typeof value === 'string' && typeof newValue === 'string') {
     return [`'${value}'`, `'${newValue}'`];
   }
@@ -14,24 +43,15 @@ const printSimpleValues = (value, newValue) => {
   return [value, newValue];
 };
 
-const printComplexValues = (value, newValue) => {
-  if (_.isObject(value)) {
-    return ['[complex value]', `'${newValue}'`];
-  }
-  if (_.isObject(newValue)) {
-    return [value, '[complex value]'];
-  }
-  return [value, newValue];
-};
-
 const plain = (tree, result = '', path = '') => {
   const {
     name, value, status, newValue, children,
   } = tree;
   if (isLeaf(tree)) {
     const nodeName = `${path}${name}`.slice(1);
-    const [printValue, printNewValue] = _.isObject(value) || _.isObject(newValue)
-      ? printComplexValues(value, newValue) : printSimpleValues(value, newValue);
+    // const [printValue, printNewValue] = _.isObject(value) || _.isObject(newValue)
+    //   ? printComplexValues(value, newValue) : printSimpleValues(value, newValue);
+    const [printValue, printNewValue] = printValues(value, newValue);
     switch (status) {
       case 'added':
         return `${result}Property '${nodeName}' was added with value: ${printValue}\n`;
