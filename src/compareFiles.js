1db25db3550e0a462eb37c578036e09cd42ed75e
Mike <k2punk@gmail.com>
2021-12-21 11:41:00 +0300

    refactor parser function

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -2,12 +2,20 @@ import chooseFormatter from './formatters/index.js';
 import buildTree from './makeTree.js';
 import parse from './parsers.js';
 import readFile from './readFile.js';
+import getFileFormat from './getFileFormat.js';
 
-const compareFiles = (file1, file2, formatName = 'stylish') => {
-  const parser = parse(file1, file2);
-  const data1 = parser(readFile(file1));
-  const data2 = parser(readFile(file2));
-  const tree = buildTree(data1, data2);
+const compareFiles = (filepath1, filepath2, formatName = 'stylish') => {
+  // const parser = parse(file1, file2);
+  // const data1 = parser(readFile(file1));
+  // const data2 = parser(readFile(file2));
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+  const format1 = getFileFormat(filepath1);
+  const format2 = getFileFormat(filepath2);
+  const file1 = parse(data1, format1);
+  const file2 = parse(data2, format2);
+  // const tree = buildTree(data1, data2);
+  const tree = buildTree(file1, file2);
   return chooseFormatter(formatName, tree);
 };
 
--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,13 @@
 import yaml from 'js-yaml';
-import * as path from 'path';
 
-export default (path1, path2) => {
-  const fileExt1 = path.extname(path1);
-  const fileExt2 = path.extname(path2);
-  const parse = fileExt1 === '.json' && fileExt2 === '.json' ? JSON.parse : yaml.load;
-  return parse;
+// export default (path1, path2) => {
+//   const fileExt1 = path.extname(path1);
+//   const fileExt2 = path.extname(path2);
+//   const parse = fileExt1 === '.json' && fileExt2 === '.json' ? JSON.parse : yaml.load;
+//   return parse;
+// };
+
+export default (data, format) => {
+  const parse = format === '.json' ? JSON.parse : yaml.load;
+  return parse(data);
 };
