c56a24d4b5f28f639ba8e8478205ab00b3fc322c
Mike <k2punk@gmail.com>
2021-12-21 12:25:04 +0300

    add getData function

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -4,9 +4,13 @@ import parse from './parsers.js';
 import readFile from './readFile.js';
 import getFileFormat from './getFileFormat.js';
 
+const getData = (filepath) => readFile(filepath);
+
 const compareFiles = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = readFile(filepath1);
-  const data2 = readFile(filepath2);
+  // const data1 = readFile(filepath1);
+  // const data2 = readFile(filepath2);
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
   const format1 = getFileFormat(filepath1);
   const format2 = getFileFormat(filepath2);
   const file1 = parse(data1, format1);
