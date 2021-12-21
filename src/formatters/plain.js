8a9fdb093b033703db5ab7d98839955fcfb5241d
Mike <k2punk@gmail.com>
2021-12-21 15:48:52 +0300

    add inner function to plain function

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,27 +8,30 @@ const printValues = (value) => {
   return printValue;
 };
 
-const plain = (tree, result = '', path = '') => {
-  const {
-    name, value, status, newValue, children,
-  } = tree;
-  if (isLeaf(tree)) {
-    const nodeName = `${path}${name}`.slice(1);
-    const printValue = printValues(value);
-    const printNewValue = printValues(newValue);
-    switch (status) {
-      case 'added':
-        return `${result}Property '${nodeName}' was added with value: ${printValue}\n`;
-      case 'removed':
-        return `${result}Property '${nodeName}' was removed\n`;
-      case 'updated':
-        return `${result}Property '${nodeName}' was updated. From ${printValue} to ${printNewValue}\n`;
-      default:
-        return '';
+const plain = (tree) => {
+  const cb = (node, result = '', path = '') => {
+    const {
+      name, value, status, newValue, children,
+    } = node;
+    if (isLeaf(node)) {
+      const nodeName = `${path}${name}`.slice(1);
+      const printValue = printValues(value);
+      const printNewValue = printValues(newValue);
+      switch (status) {
+        case 'added':
+          return `${result}Property '${nodeName}' was added with value: ${printValue}\n`;
+        case 'removed':
+          return `${result}Property '${nodeName}' was removed\n`;
+        case 'updated':
+          return `${result}Property '${nodeName}' was updated. From ${printValue} to ${printNewValue}\n`;
+        default:
+          return '';
+      }
     }
-  }
-  const res = children.map((item) => plain(item, result, `${path}${name}.`)).join('');
-  return (path === '') ? res.slice(0, -1) : res;
+    const res = children.map((item) => cb(item, result, `${path}${name}.`)).join('');
+    return (path === '') ? res.slice(0, -1) : res;
+  };
+  return cb(tree);
 };
 
 export default plain;
