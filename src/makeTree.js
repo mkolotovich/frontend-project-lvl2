af1a0ff9fb9b759db51e48883d7280f0d6d5a9b2
Mike <k2punk@gmail.com>
2021-12-22 15:37:55 +0300

    refactor makeTree module

--- src/makeTree.js
+++ src/makeTree.js
@@ -1,27 +1,28 @@
 import _ from 'lodash';
 
-const getKeys = (obj, keys, path, obj1) => {
-  const cb = ([key, value]) => {
-    if (!_.isObject(value)) {
-      keys.add(`${path}.${key}`);
-    } else {
-      const fullPath = `${path}.${key}`;
-      keys.add(fullPath);
-      if (!_.isObject(_.get(obj1, fullPath.slice(1)))) {
-        return [];
-      }
-      return getKeys(value, keys, fullPath, obj1);
-    }
-    return keys;
-  };
-  const objKeys = Object.entries(obj);
-  objKeys.map((child) => cb(child));
-  return keys;
-};
+// const getKeys = (obj, keys, path, obj1) => {
+//   const cb = ([key, value]) => {
+//     if (!_.isObject(value)) {
+//       keys.add(`${path}.${key}`);
+//     } else {
+//       const fullPath = `${path}.${key}`;
+//       keys.add(fullPath);
+//       if (!_.isObject(_.get(obj1, fullPath.slice(1)))) {
+//         return [];
+//       }
+//       return getKeys(value, keys, fullPath, obj1);
+//     }
+//     return keys;
+//   };
+//   const objKeys = Object.entries(obj);
+//   objKeys.map((child) => cb(child));
+//   return keys;
+// };
 
 const isValueObject = (node, file1, file2) => {
-  if (_.isPlainObject(_.get(file1, node.slice(1)))
-  && _.isPlainObject(_.get(file2, node.slice(1)))) {
+  // if (_.isPlainObject(_.get(file1, node.slice(1)))
+  // && _.isPlainObject(_.get(file2, node.slice(1)))) {
+  if (_.isPlainObject(_.get(file1, node)) && _.isPlainObject(_.get(file2, node))) {
     return true;
   }
   return false;
@@ -31,90 +32,105 @@ const chooseType = (node, file1, file2) => {
   if (isValueObject(node, file1, file2)) {
     return 'nested';
   }
-  if (_.get(file1, node.slice(1)) === _.get(file2, node.slice(1))) {
+  // if (_.get(file1, node.slice(1)) === _.get(file2, node.slice(1))) {
+  if (_.get(file1, node) === _.get(file2, node)) {
     return 'unchanged';
   }
-  if (_.has(file1, node.slice(1)) && _.has(file2, node.slice(1))
-  && _.get(file1, node.slice(1)) !== _.get(file2, node.slice(1))) {
+  // if (_.has(file1, node.slice(1)) && _.has(file2, node.slice(1))
+  // && _.get(file1, node.slice(1)) !== _.get(file2, node.slice(1))) {
+  if (_.has(file1, node) && _.has(file2, node) && _.get(file1, node) !== _.get(file2, node)) {
     return 'updated';
   }
-  if (_.has(file1, node.slice(1))) {
+  // if (_.has(file1, node.slice(1))) {
+  if (_.has(file1, node)) {
     return 'removed';
   }
   return 'added';
 };
 
 const makeNode = (name, children, file1, file2) => ({
-  name: _.last(name.slice(1).split('.')),
+  // name: _.last(name.slice(1).split('.')),
+  name,
   children,
   type: 'node',
   status: chooseType(name, file1, file2),
 });
 
 const getValue = (node, file1, file2) => {
-  if (_.has(file1, node.slice(1))) {
-    return _.get(file1, node.slice(1));
+  // if (_.has(file1, node.slice(1))) {
+  if (_.has(file1, node)) {
+    // return _.get(file1, node.slice(1));
+    return _.get(file1, node);
   }
-  return _.get(file2, node.slice(1));
+  // return _.get(file2, node.slice(1));
+  return _.get(file2, node);
 };
 
 const makeLeaf = (name, file1, file2) => ({
-  name: _.last(name.slice(1).split('.')),
+  // name: _.last(name.slice(1).split('.')),
+  name,
   type: 'leaf',
   status: chooseType(name, file1, file2),
   value: getValue(name, file1, file2),
-  newValue: _.get(file2, name.slice(1)),
+  // newValue: _.get(file2, name.slice(1)),
+  newValue: _.get(file2, name),
 });
 
-const makeTree = (keys, file1, file2, acc) => {
+// const makeTree = (keys, file1, file2, acc) => {
+//   if (keys.length === 0) {
+//     return _.sortBy(acc, ['name']);
+//   }
+//   const [header] = keys;
+//   const rest = keys.slice(1);
+//   if (header.slice(1).includes('.')) {
+//     if (isValueObject(header, file1, file2)) {
+//       return makeTree([], file1, file2,
+//         [...acc, makeNode(header, makeTree(rest, file1, file2, []), file1, file2)]);
+//     } if (chooseType(header, file1, file2) === 'updated') {
+//       return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2)]);
+//     }
+//     return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2)]);
+//   }
+//   if (chooseType(header, file1, file2) !== 'nested') {
+//     return makeLeaf(header, file1, file2);
+//   }
+//   return makeTree(rest, file1, file2, []);
+// };
+
+const makeTree = (keys, file1, file2, acc = []) => {
   if (keys.length === 0) {
     return _.sortBy(acc, ['name']);
   }
   const [header] = keys;
   const rest = keys.slice(1);
-  if (header.slice(1).includes('.')) {
-    if (isValueObject(header, file1, file2)) {
-      return makeTree([], file1, file2,
-        [...acc, makeNode(header, makeTree(rest, file1, file2, []), file1, file2)]);
-    } if (chooseType(header, file1, file2) === 'updated') {
-      return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2)]);
-    }
-    return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2)]);
+  if (chooseType(header, file1, file2) === 'nested') {
+    const subKeys1 = _.get(file1, header);
+    const subKeys2 = _.get(file2, header);
+    const innerKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
+    const keyIsObject = innerKeys.filter((el) => isValueObject(el, subKeys1, subKeys2));
+    const keysNotObject = innerKeys.filter((el) => !isValueObject(el, subKeys1, subKeys2));
+    const sortedKeys = [...keysNotObject, ...keyIsObject];
+    return makeTree([], subKeys1, subKeys2,
+      [...acc, makeNode(header, makeTree(sortedKeys, subKeys1, subKeys2, []), subKeys1, subKeys2)]);
   }
   if (chooseType(header, file1, file2) !== 'nested') {
-    return makeLeaf(header, file1, file2);
+    return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2)]);
   }
-  return makeTree(rest, file1, file2, []);
+  return [];
 };
 
-const sort = (item, group) => _.sortBy(item, (x) => {
-  const itemsLength = item.map((el) => el.split('.').length);
-  const maxLength = _.max(itemsLength);
-  const index = itemsLength.indexOf(maxLength) - 1;
-  const filtered = item.filter((el, ind) => ind === index || el.split('.').length === maxLength);
-  const maxKeyLength = _.max(item.filter((el) => el.slice(group.length).split('.').length === 2).map((elem) => elem.slice(group.length + 1).length));
-  const sortKeys = x.split('.');
-  if (filtered.includes(x)) {
-    return sortKeys.map((i) => _.padEnd(i, maxKeyLength + 1)).join('');
-  }
-  return sortKeys.map((i) => _.padStart(i, maxKeyLength + 1)).join('');
-});
-
 const buildTree = (parsedData1, parsedData2) => {
-  const keys = new Set();
-  getKeys(parsedData1, keys, '', parsedData2);
-  getKeys(parsedData2, keys, '', parsedData1);
-  const keysArray = Array.from(keys);
-  const sortedKeys = _.sortBy(keysArray);
-  const groups = sortedKeys.filter((item) => !item.slice(1).includes('.'));
-  const groupKeys = groups.map((item) => sortedKeys.filter((el) => el.includes(item)));
-  const res = groupKeys.map((item) => {
-    const [group] = item;
-    if (Array.isArray(makeTree(item, parsedData1, parsedData2))) {
-      const sorted = sort(item, group);
-      return makeNode(group, makeTree(sorted, parsedData1, parsedData2), parsedData1, parsedData2);
+  const keys = _.union(Object.keys(parsedData1), Object.keys(parsedData2));
+  // const res = makeTree(keys, parsedData1, parsedData2);
+  const res = keys.map((item) => {
+    const subKeys1 = _.get(parsedData1, item);
+    const subKeys2 = _.get(parsedData2, item);
+    if (chooseType(item, parsedData1, parsedData2) === 'nested') {
+      const itemKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
+      const sortedKeys = _.sortBy(itemKeys);
+      return makeNode(item, makeTree(sortedKeys, subKeys1, subKeys2), parsedData1, parsedData2);
     }
-    return makeTree(item, parsedData1, parsedData2);
+    return makeLeaf(item, parsedData1, parsedData2);
   });
   const tree = makeNode('', res);
   return tree;
