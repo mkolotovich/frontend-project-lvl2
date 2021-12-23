bfea7ed392ad83c0df5d8ee74cfe385d4689c34c
Mike <k2punk@gmail.com>
2021-12-23 14:49:27 +0300

    remove extra condition in makeTree function

--- src/makeTree.js
+++ src/makeTree.js
@@ -27,28 +27,23 @@ const makeLeaf = (name, file1, file2, type) => ({
   newValue: _.get(file2, name),
 });
 
-const makeTree = (keys, file1, file2, acc = []) => {
-  if (keys.length === 0) {
-    return _.sortBy(acc, ['name']);
+const makeTree = (keys, file1, file2) => keys.map((el) => {
+  if (isValueObject(el, file1, file2)) {
+    const subKeys1 = _.get(file1, el);
+    const subKeys2 = _.get(file2, el);
+    const innerKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
+    const sortedKeys = _.sortBy(innerKeys);
+    return makeNode(el, makeTree(sortedKeys, subKeys1, subKeys2, []), 'nested');
   }
-  return keys.map((el) => {
-    if (isValueObject(el, file1, file2)) {
-      const subKeys1 = _.get(file1, el);
-      const subKeys2 = _.get(file2, el);
-      const innerKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
-      const sortedKeys = _.sortBy(innerKeys);
-      return makeNode(el, makeTree(sortedKeys, subKeys1, subKeys2, []), 'nested');
-    }
-    if (_.get(file1, el) === _.get(file2, el)) {
-      return makeLeaf(el, file1, file2, 'unchanged');
-    } if (_.has(file1, el) && _.has(file2, el) && _.get(file1, el) !== _.get(file2, el)) {
-      return makeLeaf(el, file1, file2, 'updated');
-    } if (_.has(file1, el)) {
-      return makeLeaf(el, file1, file2, 'removed');
-    }
-    return makeLeaf(el, file1, file2, 'added');
-  });
-};
+  if (_.get(file1, el) === _.get(file2, el)) {
+    return makeLeaf(el, file1, file2, 'unchanged');
+  } if (_.has(file1, el) && _.has(file2, el) && _.get(file1, el) !== _.get(file2, el)) {
+    return makeLeaf(el, file1, file2, 'updated');
+  } if (_.has(file1, el)) {
+    return makeLeaf(el, file1, file2, 'removed');
+  }
+  return makeLeaf(el, file1, file2, 'added');
+});
 
 const buildTree = (parsedData1, parsedData2) => {
   const keys = _.union(Object.keys(parsedData1), Object.keys(parsedData2));
