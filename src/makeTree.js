7cf4856e729b434a89c99cebe6fe425d40d050f1
Mike <k2punk@gmail.com>
2021-12-23 14:41:01 +0300

    refactor makeTree function

--- src/makeTree.js
+++ src/makeTree.js
@@ -27,31 +27,54 @@ const makeLeaf = (name, file1, file2, type) => ({
   newValue: _.get(file2, name),
 });
 
+// const makeTree = (keys, file1, file2, acc = []) => {
+//   if (keys.length === 0) {
+//     return _.sortBy(acc, ['name']);
+//   }
+//   const [header] = keys;
+//   const rest = keys.slice(1);
+//   if (isValueObject(header, file1, file2)) {
+//     const subKeys1 = _.get(file1, header);
+//     const subKeys2 = _.get(file2, header);
+//     const innerKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
+//     const keyIsObject = innerKeys.filter((el) => isValueObject(el, subKeys1, subKeys2));
+//     const keysNotObject = innerKeys.filter((el) => !isValueObject(el, subKeys1, subKeys2));
+//     const sortedKeys = [...keysNotObject, ...keyIsObject];
+//     return makeTree(rest, file1, file2,
+//       [...acc, makeNode(header, makeTree(sortedKeys, subKeys1, subKeys2, []), 'nested')]);
+//   }
+//   if (_.get(file1, header) === _.get(file2, header)) {
+//     return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'unchanged')]);
+//   } if (_.has(file1, header) && _.has(file2, header)
+//   && _.get(file1, header) !== _.get(file2, header)) {
+//     return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'updated')]);
+//   } if (_.has(file1, header)) {
+//     return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'removed')]);
+//   }
+//   return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'added')]);
+// };
+
 const makeTree = (keys, file1, file2, acc = []) => {
   if (keys.length === 0) {
     return _.sortBy(acc, ['name']);
   }
-  const [header] = keys;
-  const rest = keys.slice(1);
-  if (isValueObject(header, file1, file2)) {
-    const subKeys1 = _.get(file1, header);
-    const subKeys2 = _.get(file2, header);
-    const innerKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
-    const keyIsObject = innerKeys.filter((el) => isValueObject(el, subKeys1, subKeys2));
-    const keysNotObject = innerKeys.filter((el) => !isValueObject(el, subKeys1, subKeys2));
-    const sortedKeys = [...keysNotObject, ...keyIsObject];
-    return makeTree(rest, file1, file2,
-      [...acc, makeNode(header, makeTree(sortedKeys, subKeys1, subKeys2, []), 'nested')]);
-  }
-  if (_.get(file1, header) === _.get(file2, header)) {
-    return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'unchanged')]);
-  } if (_.has(file1, header) && _.has(file2, header)
-  && _.get(file1, header) !== _.get(file2, header)) {
-    return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'updated')]);
-  } if (_.has(file1, header)) {
-    return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'removed')]);
-  }
-  return makeTree(rest, file1, file2, [...acc, makeLeaf(header, file1, file2, 'added')]);
+  return keys.map((el) => {
+    if (isValueObject(el, file1, file2)) {
+      const subKeys1 = _.get(file1, el);
+      const subKeys2 = _.get(file2, el);
+      const innerKeys = _.union(Object.keys(subKeys1), Object.keys(subKeys2));
+      const sortedKeys = _.sortBy(innerKeys);
+      return makeNode(el, makeTree(sortedKeys, subKeys1, subKeys2, []), 'nested');
+    }
+    if (_.get(file1, el) === _.get(file2, el)) {
+      return makeLeaf(el, file1, file2, 'unchanged');
+    } if (_.has(file1, el) && _.has(file2, el) && _.get(file1, el) !== _.get(file2, el)) {
+      return makeLeaf(el, file1, file2, 'updated');
+    } if (_.has(file1, el)) {
+      return makeLeaf(el, file1, file2, 'removed');
+    }
+    return makeLeaf(el, file1, file2, 'added');
+  });
 };
 
 const buildTree = (parsedData1, parsedData2) => {
