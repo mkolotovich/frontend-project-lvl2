9432767017be305c1bdf3aed7450d8457e6e8a3e
Mike <k2punk@gmail.com>
2021-12-29 11:35:08 +0300

    refactor compareFiles module

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -4,15 +4,15 @@ import parse from './parsers.js';
 import readFile from './readFile.js';
 import getFileFormat from './getFileFormat.js';
 
-const getData = (filepath) => readFile(filepath);
+const getData = (filePath) => {
+  const data = readFile(filePath);
+  const format = getFileFormat(filePath);
+  return parse(data, format);
+};
 
-const compareFiles = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = getData(filepath1);
-  const data2 = getData(filepath2);
-  const format1 = getFileFormat(filepath1);
-  const format2 = getFileFormat(filepath2);
-  const parsedData1 = parse(data1, format1);
-  const parsedData2 = parse(data2, format2);
+const compareFiles = (filePath1, filePath2, formatName = 'stylish') => {
+  const parsedData1 = getData(filePath1);
+  const parsedData2 = getData(filePath2);
   const tree = buildTree(parsedData1, parsedData2);
   return chooseFormatter(formatName, tree);
 };
