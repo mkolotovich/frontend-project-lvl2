0f1fcd8ff7966dee1be831de9a9b3bd795f8ad0c
Mike <k2punk@gmail.com>
2021-12-30 12:58:24 +0300

    fix naming in compareFiles module

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -1,4 +1,4 @@
-import chooseFormatter from './formatters/index.js';
+import formatData from './formatters/index.js';
 import buildTree from './makeTree.js';
 import parse from './parsers.js';
 import readFile from './readFile.js';
@@ -14,7 +14,7 @@ const compareFiles = (filePath1, filePath2, formatName = 'stylish') => {
   const parsedData1 = getData(filePath1);
   const parsedData2 = getData(filePath2);
   const tree = buildTree(parsedData1, parsedData2);
-  return chooseFormatter(formatName, tree);
+  return formatData(formatName, tree);
 };
 
 export default compareFiles;
